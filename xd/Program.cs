343902adfd356e3209c56b531353aa998550baf7
Laboratorio J401 TLS -JP <labj401-tlsjp@campustls.local>
2026-09-23 16:50:52 -0500

    se realiso una aventura texto con combate RPG

--- xd/Program.cs
+++ xd/Program.cs
@@ -1,15 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace xd
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string nombre;
+        int vida = 100;
+        int vidaEnemigo = 50;
+        int opcion;
+
+        Console.WriteLine("=== AVENTURA RPG ===");
+
+        Console.Write("Escribe tu nombre: ");
+        nombre = Console.ReadLine();
+
+        Console.WriteLine("\nHola " + nombre);
+        Console.WriteLine("¡Un Goblin apareció!");
+        Console.WriteLine("Comienza el combate.");
+
+        while (vida > 0 && vidaEnemigo > 0)
         {
+            Console.WriteLine("\nTu vida: " + vida);
+            Console.WriteLine("Vida del Goblin: " + vidaEnemigo);
+
+            Console.WriteLine("\n¿Qué quieres hacer?");
+            Console.WriteLine("1. Atacar");
+            Console.WriteLine("2. Curarse");
+            Console.Write("Elige: ");
+
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                vidaEnemigo = vidaEnemigo - 10;
+
+                Console.WriteLine("\nAtacaste al Goblin.");
+                Console.WriteLine("Le hiciste 10 de daño.");
+
+                if (vidaEnemigo > 0)
+                {
+                    vida = vida - 5;
+
+                    Console.WriteLine("El Goblin te atacó.");
+                    Console.WriteLine("Perdiste 5 de vida.");
+                }
+            }
+            else if (opcion == 2)
+            {
+                vida = vida + 10;
+
+                if (vida > 100)
+                {
+                    vida = 100;
+                }
+
+                Console.WriteLine("\nTe curaste 10 de vida.");
+            }
+            else
+            {
+                Console.WriteLine("\nOpción incorrecta.");
+            }
         }
+
+        if (vidaEnemigo <= 0)
+        {
+            Console.WriteLine("\n=== VICTORIA ===");
+            Console.WriteLine("Derrotaste al Goblin.");
+        }
+        else
+        {
+            Console.WriteLine("\n=== DERROTA ===");
+            Console.WriteLine("Te quedaste sin vida.");
+        }
+
+        Console.WriteLine("\nPresiona una tecla para salir...");
+        Console.ReadKey();
     }
 }
